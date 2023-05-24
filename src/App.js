db9d3c23d0c9cce358ebc8a5b81b588e4b676abe
Darion George <Barry0719@gmail.com>
2023-05-24 05:32:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,28 +3,62 @@ import "./App.css";
 
 const Calculator = () => {
 const [display, setDisplay] = useState('0');
+const [storedValue, setStoredValue] = useState(null);
+const [operator, setOperator] = useState(null);
+
 
 const handleDisplayUpdate = (event) => {
   const clickedValue = event.target.value;
-  setDisplay(prevDisplay => {
-    if (prevDisplay === '0') {
-      return clickedValue;
-    } else {
-      return prevDisplay + clickedValue;
+  if (clickedValue === '.') {
+    if (!display.includes('.')) {
+      setDisplay(prevDisplay => prevDisplay + clickedValue);
     }
-  });
-}
+  } else {
+    setDisplay(prevDisplay => {
+      if (prevDisplay === '0') {
+        return clickedValue;
+      } else {
+        return prevDisplay + clickedValue;
+      }
+    });
+  }
+};
 
 const handleClear = () => {
   setDisplay('0');
+  setStoredValue(null);
+  setOperator(null);
 };
 
-const operators = ['+', '-', '*', '/'];
 const handleOperator = (event) => {
-
+  const clickedOperator = event.target.value;
+  if (clickedOperator === '-' && display === '0') {
+    setDisplay('-');
+  } else if (operator && storedValue) {
+    const expression = storedValue + operator + display;
+    const result = eval(expression);
+    console.log(result);
+    setStoredValue(result);
+    setOperator(clickedOperator);
+    setDisplay(result.toString());
+  } else {
+    setStoredValue(display);
+    setOperator(clickedOperator);
+    setDisplay('0');
+  }
 };
 
-
+const handleEquals = () => {
+  if (operator && storedValue) {
+    const expression = storedValue + operator + display;
+    const result = eval(expression);
+    console.log(result);
+    const formattedResult = result % 1 !== 0 ? result.toFixed(4) : result.toFixed(0);
+    setDisplay(formattedResult.toString());
+    setStoredValue(null);
+    setOperator(null);
+  }
+};
 
 
 
@@ -32,7 +66,7 @@ const handleOperator = (event) => {
     <>
     <div id="display">{!display ? '0' : display}</div>
     <div>
-      <button id='equals' value={'='}>=</button>
+      <button id='equals' value={'='} onClick={handleEquals}>=</button>
       <button id='zero' value={'0'} onClick={handleDisplayUpdate}>0</button>
       <button id='one' value={'1'} onClick={handleDisplayUpdate}>1</button>
       <button id='two' value={'2'} onClick={handleDisplayUpdate}>2</button>
