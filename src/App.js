97bc69ec791c29935efea6d5b750b50ba5c2c8dd
Darion George <Barry0719@gmail.com>
2023-05-24 07:03:44 -0400

    Barebone styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const Calculator = () => {
@@ -35,58 +35,88 @@ const handleOperator = (event) => {
   if (clickedOperator === '-' && display === '0') {
     setDisplay('-');
   } else if (operator && storedValue) {
-    const expression = storedValue + operator + display;
-    const result = eval(expression);
-    console.log(result);
-    setStoredValue(result);
-    setOperator(clickedOperator);
-    setDisplay(result.toString());
+    if (isOperator(operator) && isOperator(clickedOperator)) {
+      // handle consecutive operators
+      if (clickedOperator === '-') {
+        setDisplay('-');
+      } else {
+        setOperator(clickedOperator);
+      }
+    } else {
+      let expression = storedValue + operator + display;
+      if (storedValue === '-' && isOperator(clickedOperator)) {
+        expression = storedValue + clickedOperator;
+        setStoredValue(expression);
+      } else {
+        // handle consecutive operators
+        if (isOperator(display) && isOperator(clickedOperator)) {
+          setOperator(clickedOperator);
+        } else {
+          const result = eval(expression);
+          setStoredValue(result);
+          if (clickedOperator === '=') {
+            let formattedResult = result % 1 !== 0 ? result.toFixed(4) : result.toFixed(0);
+            formattedResult = parseFloat(formattedResult).toString(); // convert to string to remove trailing zeros
+            setDisplay(formattedResult);
+            setOperator(null);
+            setStoredValue(null);
+          } else {
+            setOperator(clickedOperator);
+            setDisplay('0');
+          }
+        }
+      }
+    }
   } else {
     setStoredValue(display);
     setOperator(clickedOperator);
     setDisplay('0');
   }
 };
-
+// 5 * - + 5
+const isOperator = (value) => {
+  return value === '+' || value === '-' || value === '*' || value === '/';
+};
 const handleEquals = () => {
   if (operator && storedValue) {
     const expression = storedValue + operator + display;
     const result = eval(expression);
     console.log(result);
-    const formattedResult = result % 1 !== 0 ? result.toFixed(4) : result.toFixed(0);
-    setDisplay(formattedResult.toString());
+    let formattedResult = result % 1 !== 0 ? result.toFixed(4) : result.toFixed(0);
+    formattedResult = parseFloat(formattedResult).toString(); // convert to string to remove trailing zeros
+    setDisplay(formattedResult);
     setStoredValue(null);
     setOperator(null);
   }
 };
 
 
-
   return (
-    <>
-    <div id="display">{!display ? '0' : display}</div>
-    <div>
-      <button id='equals' value={'='} onClick={handleEquals}>=</button>
-      <button id='zero' value={'0'} onClick={handleDisplayUpdate}>0</button>
-      <button id='one' value={'1'} onClick={handleDisplayUpdate}>1</button>
-      <button id='two' value={'2'} onClick={handleDisplayUpdate}>2</button>
-      <button id='two' value={'2'} onClick={handleDisplayUpdate}>2</button>
-      <button id='three' value={'3'} onClick={handleDisplayUpdate}>3</button>
-      <button id='four' value={'4'} onClick={handleDisplayUpdate}>4</button>
-      <button id='five' value={'5'} onClick={handleDisplayUpdate}>5</button>
-      <button id='six' value={'6'} onClick={handleDisplayUpdate}>6</button>
-      <button id='seven' value={'7'} onClick={handleDisplayUpdate}>7</button>
-      <button id='eight' value={'8'} onClick={handleDisplayUpdate}>8</button>
-      <button id='nine' value={'9'} onClick={handleDisplayUpdate}>9</button>
-      <button id='add' value={'+'} onClick={handleOperator}>+</button>
-      <button id='subtract' value={'-'} onClick={handleOperator}>-</button>
-      <button id='multiply' value={'*'} onClick={handleOperator}>*</button>
-      <button id='divide' value={'/'} onClick={handleOperator}>/</button>
-      <button id='clear' value={'C'} onClick={handleClear}>C</button>
-      <button id='decimal' value={'.'} onClick={handleDisplayUpdate}>.</button>
-      <button id='negative' value={'-'}>-</button>
-    </div>
-    </>
+<div id="calculator">
+  <div>
+    <h1>Javascript Calculator</h1>
+  </div>
+  <div id="display">{!display ? '0' : display}</div>
+  <div id="button-container">
+    <button id='seven' value={'7'} onClick={handleDisplayUpdate}>7</button>
+    <button id='eight' value={'8'} onClick={handleDisplayUpdate}>8</button>
+    <button id='nine' value={'9'} onClick={handleDisplayUpdate}>9</button>
+    <button id='divide' value={'/'} onClick={handleOperator}>/</button>
+    <button id='four' value={'4'} onClick={handleDisplayUpdate}>4</button>
+    <button id='five' value={'5'} onClick={handleDisplayUpdate}>5</button>
+    <button id='six' value={'6'} onClick={handleDisplayUpdate}>6</button>
+    <button id='multiply' value={'*'} onClick={handleOperator}>*</button>
+    <button id='one' value={'1'} onClick={handleDisplayUpdate}>1</button>
+    <button id='two' value={'2'} onClick={handleDisplayUpdate}>2</button>
+    <button id='three' value={'3'} onClick={handleDisplayUpdate}>3</button>
+    <button id='subtract' value={'-'} onClick={handleOperator}>-</button>
+    <button id='zero' value={'0'} onClick={handleDisplayUpdate}>0</button>
+    <button id='decimal' value={'.'} onClick={handleDisplayUpdate}>.</button>
+    <button id='equals' value={'='} onClick={handleEquals}>=</button>
+    <button id='add' value={'+'} onClick={handleOperator}>+</button>
+    <button id='clear' value={'C'} onClick={handleClear}>C</button>
+  </div>
+</div>
   )
 }
 export default Calculator;
